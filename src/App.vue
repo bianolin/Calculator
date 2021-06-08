0b6c1a87a56fed0a48910855c6fac354746f061d
bianolin@gmail.com <bianolin@gmail.com>
2021-06-08 22:41:42 +0800

    Updated partition

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div class="AppWrap">
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
+.AppWrap {
+  height: 100vh;
+  width: 100vw;
+  max-height: 100vh;
+  max-width: 960px;
+  margin: auto;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/views/Calculator.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="Main">
+    <div class="CalArea">
+
+    </div>
+    <div class="LogArea">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.Main{
+  height: 100%;
+  width: 100%;
+  background-color: lightgray;
+  display: flex;
+  .CalArea{
+    flex: 5;
+    height: 100%;
+    background-color:lightgreen
+  }
+  .LogArea{
+    height: 100%;
+    background-color:lightsalmon;
+    flex: 2;
+  }
+}
+</style>
